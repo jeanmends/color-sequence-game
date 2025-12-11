795d594112fe9c5d31b8e196873722fc82da27b2
Jean <{99772124}+{jeanmends}@users.noreply.github.com>
2025-12-11 11:52:53 -0300

    inicial project

---
+++ index.js
@@ -0,0 +1,91 @@
+const colors = document.querySelectorAll(".inner");
+let score = document.querySelector(".score");
+
+let track = [];
+let countClick = 0;
+let numberOfClicks = 0;
+
+function genareteRandomColor(){
+    const random = Math.floor(Math.random() * colors.length);
+    track.push(random);
+    showColors();
+}
+
+function setColors(element){
+  const time = 1000;
+  return new Promise(resolve => {
+    setTimeout(() => {
+      element.style.transition = 'opacity 2000ms linear';
+      element.style.opacity = 1;
+
+      setTimeout(() => {
+        element.style.opacity = 0.2;
+        setTimeout(resolve, 2000); 
+      }, time + 500);
+    }, time);
+  });
+}
+
+const showColors = async () => {
+  score.innerHTML = 'Wait the colors and pay attention!';
+  score.style.visibility = 'visible';
+  for (let i = 0; i <  track.length; i++){
+    await setColors(colors[track[i]]);
+  }
+
+    countClick = track.length;
+    playGame(track, countClick);
+};
+
+async function playGame(array, countClick){
+
+  let count = countClick;
+  score.innerHTML = 'Now you can click!';
+  for (let i = 0; i < count; i++){
+    const clickedIndex = await waitForClick();
+    const awnser = clickedIndex; 
+    if (awnser == array[i]){
+          numberOfClicks++;
+          console.log("acertou");
+        }else{
+          console.log("errou!")
+          gameEnd();
+          return false;        
+    }
+  }
+
+   genareteRandomColor();
+
+}
+
+function gameEnd(){
+  alert("acabou!")
+  
+  score.style.visibility = 'visible'
+  score.innerHTML = `Final Score: ${track.length - 1}`;
+  numberOfClicks = 0;
+}
+
+//button to start the game
+
+const buttonStartGame = document.querySelector("button");
+
+buttonStartGame.addEventListener('click', () =>{
+  let score = document.querySelector(".score");
+  score.style.visibility = 'hidden';
+  genareteRandomColor();
+})
+
+function waitForClick() {
+  return new Promise(resolve => {
+    // adiciona listener a cada square; remove todos depois do primeiro clique
+    colors.forEach((el, i) => {
+      const handler = () => {
+        // remove todos os handlers
+        colors.forEach((e) => e.removeEventListener('click', handler));
+        resolve(i);
+      };
+      el.addEventListener('click', handler);
+    });
+  });
+}
